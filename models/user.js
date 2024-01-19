983b15ba1b6e96ac54605341903d03cd980d200b
Paul Shephard <paulshephard598@gmail.com>
2024-01-19 13:23:24 -0500

    create user model, routes, and controller & manually test routes

---
+++ models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt') 
+const jwt = require('jsonwebtoken') 
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+})
+
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 8)
+  }
+  next()
+})
+
+userSchema.methods.generateAuthToken = async function() {
+  const token = jwt.sign({ _id: this._id }, 'secret')
+  return token
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
